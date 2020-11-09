a1be95c2c71aa962afe1313b59fe096700c6bf83
Sanyi0001 <70522920+Sanyi0001@users.noreply.github.com>
2020-11-09 15:16:40 +0100

    created draft

--- build/app.js
+++ build/app.js
@@ -1,22 +1,67 @@
-class Animal {
-    constructor(name, legs, sound) {
-        this.name = name;
-        this.legs = legs;
-        this.sound = sound;
+window.addEventListener("load", init);
+const lettersInDOM = document.querySelector("#letters");
+const attemptInDOM = document.querySelector("#attempt");
+let attempts = 5;
+attemptInDOM.innerText = attempts.toString();
+let word = "dish";
+let lettersInWord = splitWord(word);
+console.log(lettersInWord);
+let guessLetterInWord = fillWithDashes();
+console.log(guessLetterInWord);
+function writeGuessedLettersToDOM() {
+    lettersInDOM.innerHTML = "";
+    guessLetterInWord.forEach(letter => {
+        let listElement = document.createElement("li");
+        listElement.innerText = letter;
+        lettersInDOM.appendChild(listElement);
+    });
+}
+function init() {
+    writeAlphabetToTheDom();
+    writeGuessedLettersToDOM();
+}
+function writeAlphabetToTheDom() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    const keyboard = document.querySelector("#keyboard");
+    alphabet.forEach(function (element, index) {
+        let divKey = document.createElement("div");
+        divKey.id = element;
+        divKey.classList.add("key");
+        divKey.innerHTML = element;
+        divKey.addEventListener("click", function () {
+            letterClicked(divKey);
+        });
+        keyboard.append(divKey);
+    });
+}
+function splitWord(word) {
+    return word.split("");
+}
+function fillWithDashes() {
+    return lettersInWord.map((element) => "-");
+}
+function letterClicked(key) {
+    let pressedKey = key.innerText;
+    if (lettersInWord.includes(pressedKey, 0)) {
+        console.log("true");
+        for (let i = 0; i < lettersInWord.length; i++) {
+            if (lettersInWord[i] == pressedKey) {
+                console.log("ok");
+                guessLetterInWord[i] = pressedKey;
+                writeGuessedLettersToDOM();
+                if (!guessLetterInWord.includes("-", 0)) {
+                    alert("You won!");
+                }
+            }
+        }
     }
-    getName() {
-        return this.name;
+    else {
+        attempts--;
+        attemptInDOM.innerText = attempts.toString();
     }
-    getLegs() {
-        return this.legs;
-    }
-    getSound() {
-        return this.sound;
+    if (attempts === 0) {
+        alert("Game over! Start again!");
+        location.reload();
     }
 }
-const animals = [
-    new Animal('dog', 4, 'woof'),
-    new Animal('cat', 4, 'meow')
-];
-animals.forEach((animal) => console.log('A %s has %s legs and goes %s!', animal.getName(), animal.getLegs(), animal.getSound()));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,100 @@
-class Animal
-{
-    private readonly name: string
-    private readonly legs: number
-    private readonly sound: string
+window.addEventListener("load", init);
 
-    public constructor(name: string, legs: number, sound: string)
-    {
-        this.name = name
-        this.legs = legs
-        this.sound = sound
+//Global variables
+const lettersInDOM: HTMLDivElement = document.querySelector("#letters");
+const attemptInDOM: HTMLDivElement = document.querySelector("#attempt");
+let attempts: number = 5;
+attemptInDOM.innerText = attempts.toString();
+
+let word: string = "dish";
+let lettersInWord: string[] = splitWord(word);
+console.log(lettersInWord);
+let guessLetterInWord: string[] = fillWithDashes();
+console.log(guessLetterInWord);
+
+/**
+ * Function to write the guessed letters tothe DOM
+ */
+function writeGuessedLettersToDOM(){
+    lettersInDOM.innerHTML = "";
+    guessLetterInWord.forEach(letter => {
+        let listElement: HTMLElement = document.createElement("li");
+        listElement.innerText = letter;
+        lettersInDOM.appendChild(listElement);
+    });
+}
+
+
+/**
+ * Function to initialize the programme
+ */
+function init() {
+  //write the alphabet keyboard to the DOM
+  writeAlphabetToTheDom();
+  writeGuessedLettersToDOM();
+}
+
+/**
+ * Function to write the alphabet keyboard to the DOM
+ */
+function writeAlphabetToTheDom() {
+  const alphabet: string[] = "abcdefghijklmnopqrstuvwxyz".split("");
+  const keyboard: HTMLDivElement = document.querySelector("#keyboard");
+  alphabet.forEach(function (element, index) {
+    let divKey: HTMLDivElement = document.createElement("div");
+    divKey.id = element;
+    divKey.classList.add("key");
+    divKey.innerHTML = element;
+    divKey.addEventListener("click", function(){
+        letterClicked(divKey);
+    })
+    keyboard.append(divKey);
+  });
+}
+
+
+/**
+ * Function to split the word to be guessed to a string of letters
+ * @param word The word to be guessed
+ */
+function splitWord(word: string):string[] {
+    return word.split("");
+}
+
+/**
+ * Function to display dashes when the program starts
+ * @returns an array with dashes, lenght: the length of the word to be guessed
+ */
+function fillWithDashes():string[]{
+    return lettersInWord.map((element) => "-");
+}
+
+/**
+ * Function to evaluate the pressed key
+ * @param key the pressed key
+ */
+function letterClicked(key: HTMLElement){
+    let pressedKey: string = key.innerText;
+    if (lettersInWord.includes(pressedKey, 0)) { // Decides if there is a match
+        console.log("true");
+        for (let i = 0; i < lettersInWord.length; i++) {
+            if (lettersInWord[i] == pressedKey) {
+                console.log("ok");
+                guessLetterInWord[i] = pressedKey;
+                writeGuessedLettersToDOM();
+                if (!guessLetterInWord.includes("-",0)) { // If you do not have any dashes left, you win
+                    alert("You won!");
+                }
+            }
+            
+        }
+    }else{
+        attempts--;
+        attemptInDOM.innerText = attempts.toString();
     }
-
-    public getName(): string
-    {
-        return this.name
-    }
-
-    public getLegs(): number
-    {
-        return this.legs
-    }
-
-    public getSound(): string
-    {
-        return this.sound
+    if (attempts === 0) { // If you run out of attempts, you lose
+        alert("Game over! Start again!");
+        location.reload(); // Reloading the page after you lose
     }
 }
 
-const animals = [
-     new Animal('dog', 4, 'woof'),
-     new Animal('cat', 4, 'meow')
-]
-
-animals.forEach(
-    (animal) => console.log(
-        'A %s has %s legs and goes %s!',
-        animal.getName(),
-        animal.getLegs(),
-        animal.getSound()
-    )
-)
